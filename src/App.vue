8d5ae19a233f76206ea1deeade9e4c4f26e0c676
markb <markbuchholz8@gmail.com>
2023-04-19 22:35:25 -0600

    second

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+<song-list></song-list>
+<play-list></play-list>
+<now-playing></now-playing>
+  </div>
+</template>
+
+<script>
+
+
+import SongList from '@/components/SongList.vue'
+import PlayList from '@/components/PlayList.vue'
+import NowPlaying from '@/components/NowPlaying.vue'
+
+
+export default {
+
+
+  name: 'App',
+  components: {
+SongList,
+PlayList,
+NowPlaying
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
